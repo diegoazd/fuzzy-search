5bdea0b8a5ac20fa96f63bcc74cd2180ac45da10
diegoazd <diegoazd@hotmail.com>
2018-03-12 21:36:29 -0600

    delete unnecesary component

--- fuzzy-search/src/App.js
+++ fuzzy-search/src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
 	
 	this.handleChange = this.handleChange.bind(this);
 	this.handleKeyPress = this.handleKeyPress.bind(this);
-	//this.fetchData = this.fetchData.bind(this);
   }
 
   fetchData(data) {
@@ -19,7 +18,8 @@ class App extends Component {
 	fetch(url) 
 	  .then(result => {
 		result.json().then(data => {
-			console.log(data);
+			console.log(data.length);
+
 		})
 	});
   }
@@ -37,15 +37,15 @@ class App extends Component {
 	}
   }
 
-  
-
   render() {
     return (
       <div>
         <h1>Fuzzy search</h1>
         <div className="content">
-          <h2>Search:</h2>
-          <input type="text" value={this.state.value} onKeyPress={this.handleKeyPress} onChange={this.handleChange} />
+          <div className="autocomplete">
+            <h2>Search:</h2>
+            <input type="text" value={this.state.value} onKeyPress={this.handleKeyPress} onChange={this.handleChange} />
+          </div>
         </div>
       </div>
     );
